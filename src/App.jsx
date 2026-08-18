131df6f047c20911492a8dfb9c19aaa4e4752761
Vini Jaiswal <thevinijaiswal@gmail.com>
2026-08-18 16:29:05 -0700

    Rebuild: replace App entry, main, and global stylesheet

--- src/App.jsx
+++ src/App.jsx
@@ -1,162 +1,25 @@
 import React from "react";
-import "./App.css"
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import OpenNavbar from "./Opengen Main Component/OpenNavbar";
-import FooterSection from "./Opengen Main Component/FooterSection";
-import Newsletter from "./Opengen Main Component/Newletter";
- import ContactRedirectBox from "./Opengen Main Component/ContactredirectBox";
-import Services from "./Opengen Main Component/ServicesSection";
- import DeveloperEngagementRate from "./Opengen Main Component/DeveloperEngagementRate";
- import HeaderSection from "./Opengen Main Component/HeaderSection";
- import PartnersGoogle from "./Opengen Main Component/Partners";
- import AdvancedService from "./Opengen Main Component/AdvancedService";
- import FreeMidCode from "./Opengen Main Component/FreeLandingCode";
- 
-
- import AboutSection from "./AboutPage Component/AboutSection";
- import AboutNewFeed from "./AboutPage Component/AboutFeed";
- import MainBrandHeaderAbout from "./AboutPage Component/MainHeader";
- 
-  
-
-
- import ServicesHero from "./ServicesPage Component/ServicesHero";
-  
- import ServiceDev from "./ServicesPage Component/Servicedev";
-
- import ServiceDev3 from "./ServicesPage Component/Servicedev3";
-
- import ServiceDev2 from "./ServicesPage Component/Sample";
-
-  
- import CommunityEvents from "./Community Component/CommunityEvents";
-
-
- import MainBrandHeaderSupport from "./Support Component/SupportMain";
- import ContactPageSection from "./Support Component/ContactSection";
-
-
- import PartnersPage from "./PartnerShip Component/Partners";
- import MainBrandHeaderPartner from "./PartnerShip Component/MainHeaderPartner";
-
-
- import CollaborativeAppsPage from "./BrandAmplification Component/Collaborative";
- import WhatWeOffer from "./BrandAmplification Component/WhatWeOffer";
- import MainBrandHeader from "./BrandAmplification Component/MainHeader";
- import ContentBrand from "./BrandAmplification Component/ContentBrand";
-
-
-  
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
 
 function App() {
-  
-
   return (
-    <>
-      <Router>
-            <Routes>
-                <Route path="/" element={
-                    <>
-                     
-                    <OpenNavbar/>
-                    <HeaderSection/>
-                    <PartnersGoogle/>
-                    <DeveloperEngagementRate/>
-                    <Services/>
-                    <FreeMidCode/>
-                    <AdvancedService/>
-                    
-                     <ContactRedirectBox/>
-                    <Newsletter/>
-                    <FooterSection/>
-                    </>
-                } />
-
-              <Route path="/about/Opengen" element={
-                    <>
-                      <OpenNavbar/>
-                     <MainBrandHeaderAbout/>
-                       <AboutSection/>
-                       
-                       <AboutNewFeed/>
-                        
-                    <FooterSection/>
-                    </>
-                } />
-
-            <Route path="/services/Opengen" element={
-                    <>
-                     <OpenNavbar/>
-                     <ServicesHero/>
-                     <ServiceDev/>
-                     <ServiceDev3/>
-                     <ServiceDev2/>
-                      
-                     
-                    <FooterSection/>  
-                    </>
-                } />
-
-                <Route path="/partnerships/Opengen" element={
-                    <>
-                     <OpenNavbar/>
-                     <MainBrandHeaderPartner/>
-                      <PartnersPage/>
-
-                    <FooterSection/>   
-                    </>
-                } />
-
-                <Route path="/brandamplification/Opengen" element={
-                    <>
-                     <OpenNavbar/>
-                     <MainBrandHeader/>
-                     <CollaborativeAppsPage/>
-                     <ContentBrand/>
-                     <WhatWeOffer/>
-                    <FooterSection/>   
-                    </>
-                } />
-
-              <Route path="/community/Opengen" element={
-                    <>
-                     <OpenNavbar/>
-                      
-                     <CommunityEvents/>
-                     
-                    <FooterSection/>
-                    </>
-                } />
-
-                 <Route path="/learn/Opengen" element={
-                    <>
-                     <OpenNavbar/>
-                    <FooterSection/>
-                    </>
-                } />
-
-
-                <Route path="/support/Opengen" element={
-                    <>
-                     <OpenNavbar/>
-                     <MainBrandHeaderSupport/>
-                     
-                     <ContactPageSection/>
-                    <FooterSection/>   
-                    </>
-                } />
-
-                 
-                </Routes>
-                </Router>
-    
- 
-       
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
-
-
- 
